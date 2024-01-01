ad8149bb19a03ddfc2d443f10975f836c81f28e7
Tamara <tamaraavgust14@gmail.com>
2024-01-01 22:42:08 +0700

    Тест двумерного массива

--- main/main.cpp
+++ main/main.cpp
@@ -52,12 +52,24 @@ int main() {
     }
     cout << endl;
 
-    int i;
+    int i,j;
     Person array1[2];
     for (i = 0; i < 2; i++)
         array1[i].input();
     for (i = 0; i < 2; i++)
         array1[i].output();
+    cout << endl;
+
+    Car array2[2][2];
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++) {
+            array2[i][j].setBrand("Toyota");
+            array2[i][j].setRate(true);
+        }
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++) {
+            array2[i][j].output();
+        }
 
     return 0;
 }
